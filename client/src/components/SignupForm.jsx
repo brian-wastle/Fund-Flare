85aad9ee5d3150cbc2e050abe463c912a7eb7ecd
brian-wastle <brian.wastle1@gmail.com>
2023-10-03 12:32:28 -0400

    project skeleton

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
---
+++ client/src/pages/ForMePage.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import {
+  Container,
+  Card,
+  Button,
+  Row,
+  Col
+} from 'react-bootstrap';
+
+import Auth from '../utils/auth';
+import { useQuery, useMutation } from '@apollo/client';
+
+
+const ForMePage = () => {
+  
+
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default ForMePage;
