1f43757fc2cbe72402e49346ac2e16ecafcb99bc
Cam <camhack67@gmail.com>
2023-10-03 14:08:35 -0400

    added login and isOrg var

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,70 +7,86 @@ import LoginForm from './LoginForm';
 import Auth from '../utils/auth';
 
 const AppNavbar = () => {
-  // set modal display state
-  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <Navbar bg='dark' variant='dark' expand='lg'>
-        <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
-            Google Books Search
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
-            <Nav className='ml-auto d-flex'>
-              <Nav.Link as={Link} to='/'>
-                Search For Books
-              </Nav.Link>
-              {/* if user is logged in show saved books and logout */}
-              {Auth.loggedIn() ? (
-                <>
-                  <Nav.Link as={Link} to='/saved'>
-                    See Your Books
-                  </Nav.Link>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {/* set modal data up */}
-      <Modal
-        size='lg'
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby='signup-modal'>
-        {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey='login'>
-          <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
-                <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey='login'>
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
-                <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
+    <Link to="/"><h1
+    className='text-5xl text-gray-400 text-center pt-8'
+    >Home</h1></Link>
+    <Link to="/login"><h1
+    className='text-2xl text-gray-400 inline p-4'
+    >Login</h1></Link>
+    <Link to="/signup"><h1
+    className='text-2xl text-gray-400 inline p-4'
+    >signup</h1></Link>
+    <Link to="/search"><h1
+    className='text-2xl text-gray-400 inline p-4'
+    >search</h1></Link>
+    <Link to="/tags"><h1
+    className='text-2xl text-gray-400 inline p-4'
+    >tags</h1></Link>
+
     </>
+    // <>
+    //   <Navbar bg='dark' variant='dark' expand='lg'>
+    //     <Container fluid>
+    //       <Navbar.Brand as={Link} to='/'>
+    //         Donation
+    //       </Navbar.Brand>
+    //       <Navbar.Toggle aria-controls='navbar' />
+    //       <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
+    //         <Nav className='ml-auto d-flex'>
+    //           <Nav.Link as={Link} to='/'>
+    //             Search For Books
+    //           </Nav.Link>
+    //           {/* if user is logged in show saved books and logout */}
+    //           {Auth.loggedIn() ? (
+    //             <>
+    //               <Nav.Link as={Link} to='/saved'>
+    //                 See Your Books
+    //               </Nav.Link>
+    //               <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+    //             </>
+    //           ) : (
+    //             <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+    //           )}
+    //         </Nav>
+    //       </Navbar.Collapse>
+    //     </Container>
+    //   </Navbar>
+    //   {/* set modal data up */}
+    //   <Modal
+    //     size='lg'
+    //     show={showModal}
+    //     onHide={() => setShowModal(false)}
+    //     aria-labelledby='signup-modal'>
+    //     {/* tab container to do either signup or login component */}
+    //     <Tab.Container defaultActiveKey='login'>
+    //       <Modal.Header closeButton>
+    //         <Modal.Title id='signup-modal'>
+    //           <Nav variant='pills'>
+    //             <Nav.Item>
+    //               <Nav.Link eventKey='login'>Login</Nav.Link>
+    //             </Nav.Item>
+    //             <Nav.Item>
+    //               <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+    //             </Nav.Item>
+    //           </Nav>
+    //         </Modal.Title>
+    //       </Modal.Header>
+    //       <Modal.Body>
+    //         <Tab.Content>
+    //           <Tab.Pane eventKey='login'>
+    //             <LoginForm handleModalClose={() => setShowModal(false)} />
+    //           </Tab.Pane>
+    //           <Tab.Pane eventKey='signup'>
+    //             <SignUpForm handleModalClose={() => setShowModal(false)} />
+    //           </Tab.Pane>
+    //         </Tab.Content>
+    //       </Modal.Body>
+    //     </Tab.Container>
+    //   </Modal>
+    // </>
   );
 };
 
