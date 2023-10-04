b5e16bc7ae3aefac99bd17886f4d25446252b49f
Cam <camhack67@gmail.com>
2023-10-03 20:00:37 -0400

    added framer motion

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import { motion } from "framer-motion"
+
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
@@ -52,49 +54,58 @@ const LoginForm = () => {
 
   return (
     <>
-      <div className='md:container 2xl:w-1/2 xl:w-3/4 p-8 m-8 mx-auto bg-light-2 drop-shadow-sm md:rounded-lg'>
 
-        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-          <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-            Something went wrong with your signup!
-          </Alert>
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0}}
+        transition={{ duration: 0.5 }}
+      >
 
-          <Form.Group className='mb-3'>
-            <Form.Label htmlFor='email'>Email</Form.Label>
-            <Form.Control
-              type='email'
-              placeholder='Your email address'
-              name='email'
-              onChange={handleInputChange}
-              value={userFormData.email}
-              required
-            />
-            <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-          </Form.Group>
+        <div className='md:container 2xl:w-1/2 xl:w-3/4 p-8 m-8 mx-auto bg-light-2 drop-shadow-sm md:rounded-lg'>
 
-          <Form.Group className='mb-3'>
-            <Form.Label htmlFor='password'>Password</Form.Label>
-            <Form.Control
-              type='password'
-              placeholder='Your password'
-              name='password'
-              onChange={handleInputChange}
-              value={userFormData.password}
-              required
-            />
-            <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-          </Form.Group>
-          <Button
-            disabled={!(userFormData.email && userFormData.password)}
-            type='submit'
-            variant='success'>
-            Submit
-          </Button>
-        </Form>
-        <Link to="/signup"><h1
-          className='text-lg text-gray-400 text-center pt-2'
-        >Don't have an account?</h1></Link>
-      </div>
+          <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+            <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+              Something went wrong with your signup!
+            </Alert>
+
+            <Form.Group className='mb-3'>
+              <Form.Label htmlFor='email'>Email</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Your email address'
+                name='email'
+                onChange={handleInputChange}
+                value={userFormData.email}
+                required
+              />
+              <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className='mb-3'>
+              <Form.Label htmlFor='password'>Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Your password'
+                name='password'
+                onChange={handleInputChange}
+                value={userFormData.password}
+                required
+              />
+              <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+            </Form.Group>
+            <Button
+              disabled={!(userFormData.email && userFormData.password)}
+              type='submit'
+              variant='success'>
+              Submit
+            </Button>
+          </Form>
+          <Link to="/signup"><h1
+            className='text-lg text-gray-400 text-center pt-2'
+          >Don't have an account?</h1></Link>
+        </div>
+
+      </motion.div>
     </>
   );
 };
