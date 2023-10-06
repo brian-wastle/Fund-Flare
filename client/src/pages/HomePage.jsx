572bfa942092dce8c03249a5035cc57f8b508283
brianmyer <myer.brian@gmail.com>
2023-10-06 10:34:30 -0400

    rendering all organizations on homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,13 +2,43 @@ import { useState, useEffect } from 'react';
 import Carousel from '../components/Carousel';
 import Auth from '../utils/auth';
 import { useQuery, useMutation } from '@apollo/client';
+import { GET_ORGANIZATIONS } from '../utils/queries'
+import { Link } from 'react-router-dom';
+
 
 
 const HomePage = () => {
+  const { loading, data } = useQuery(GET_ORGANIZATIONS);
+  const organizations = data?.getOrganizations || {};
+  console.log(organizations)
+  if (loading) {
+    return <p>Still Loading...</p>
+  }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); // Generate a random index
+      // Swap array[i] and array[j]
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+const mutableOrganizations = [...organizations]
+shuffleArray(mutableOrganizations);
+console.log(mutableOrganizations)
 
   return (
     <>
+      {mutableOrganizations.map((organization) => {
+        return (
+          <div key={organization.id}>
+            <Link to={`/organization/${organization._id}`}>{organization.name}</Link>
+            <p>{organization.description}</p>
+            <p><img src={organization.image} alt="organization profile image" /></p>
+            <a href={organization.link}>{organization.link}</a>
 
+          </div>
+        );
+      })}
     </>
   );
 };
