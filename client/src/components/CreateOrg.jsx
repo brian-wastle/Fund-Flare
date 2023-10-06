85356d6da457070b747899f8e31090b5dcc6c90a
brian-wastle <brian.wastle1@gmail.com>
2023-10-06 14:29:49 -0400

    updated profile page

--- client/src/components/CreateOrg.jsx
+++ client/src/components/CreateOrg.jsx
@@ -64,62 +64,93 @@ console.log(userFormData)
 
   return (
     <>
-      <div className='md:container 2xl:w-1/4 xl:w-1/3 lg:w-1/2 md:w-2/3 p-8 my-40 mx-auto bg-light-2 drop-shadow-2xl md:rounded-md'>
-        <form onSubmit={handleFormSubmit} className='form login-form flex flex-col justify-center items-center'>
-          <input
-            className='font-secondary form-input text-center p-2 m-2 focus:outline-none rounded-lg bg-light-1'
-            type="name"
-            placeholder="name"
-            name="name"
-            onChange={handleInputChange}
-            value={userFormData.name}
-            required
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <p>Organization Name:</p>
+        <input
+          type="name"
+          placeholder="name"
+          name="name"
+          onChange={handleInputChange}
+          value={userFormData.name}
+          required
+        />
+        <br />
+        <p>Organization Description:</p>
+        <textarea
+          type="description"
+          placeholder="description"
+          name="description"
+          onChange={handleInputChange}
+          value={userFormData.description}
+          required
+        />
+        <br />
+        <p>Upload Image:</p>
+        <p>{imageName}</p>
+        {showOverlay === "show" ? (
+          <PickerOverlay
+            apikey={"AAAsSWvCQS2kKNL67CmbAz"}
+            onUploadDone={(res) => {
+              setImageName(res.filesUploaded[0].filename);
+              setUrlName(res.filesUploaded[0].url);
+              console.log(userImageUrl);
+              setShowOverlay("hidden");
+            }}
+            pickerOptions={{
+              onOpen: () => {
+                console.log("test");
+              },
+              onCancel: () => {
+                setShowOverlay("hidden");
+                console.log(showOverlay);
+              },
+              onClose: () => {
+                setShowOverlay("hidden");
+                console.log(showOverlay);
+              },
+            }}
           />
-          <br />
-          <textarea
-            type="description"
-            placeholder="description"
-            name="description"
-            onChange={handleInputChange}
-            value={userFormData.description}
-            required
-          />
-          <br />
-          <input
-            className='font-secondary form-input text-center p-2 m-2 focus:outline-none rounded-lg bg-light-1'
-            type="orgImage"
-            placeholder="image"
-            name="orgImage"
-            onChange={handleInputChange}
-            value={userFormData.orgImage}
-            required
-          />
-          <br />
-          <input
-            type="link"
-            placeholder="link"
-            name="link"
-            onChange={handleInputChange}
-            value={userFormData.link}
-            required
-          />
-          <br />
-          <button
-            disabled={
-              !(
-                userFormData.name &&
-                userFormData.description &&
-                userFormData.orgImage &&
-                userFormData.link
-              )
-            }
-            type="submit"
-            variant="success"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
+        ) : (
+          <button onClick={handleImageUpload}>Choose Image</button>
+        )}
+        <br />
+        <p>Link:</p>
+        <input
+          type="link"
+          placeholder="link"
+          name="link"
+          onChange={handleInputChange}
+          value={userFormData.link}
+          required
+        />
+        <br />
+        <p>Fundraising Goal:</p>
+        <input
+          type="number"
+          placeholder="10"
+          step="5"
+          name="amount"
+          onChange={handleInputChange}
+        />{" "}
+        dollars
+        <br />
+        <button
+          disabled={
+            !(
+              userFormData.name &&
+              userFormData.description &&
+              imageName &&
+              userFormData.link
+            )
+          }
+          type="submit"
+          variant="success"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
     </>
   );
   
