d630dc51b58e3173b75942cd14f550668cc4cc7e
Cam <camhack67@gmail.com>
2023-10-08 12:50:29 -0400

    carousel tweaks

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -2,44 +2,66 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from "framer-motion"
 import { useQuery, useMutation } from '@apollo/client';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const Carousel = ({orgs}) => {
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Autoplay, Mousewheel, Pagination } from 'swiper/modules';
 
-    const [activeIndex, setActiveIndex] = useState(0);
-    console.log(orgs)
-    const organizationData = orgs.map((org) => ({
-      id: org._id,
-      image: org.image,
-      title: org.name,
-    }));
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setActiveIndex((prevIndex) => (prevIndex + 1) % organizationData.length);
-      }, 2000)
-  
-      return () => clearInterval(interval)
-    }, [organizationData.length])
-  
-    return (
-      <div className='absolute right-0 bottom-0'>
-        <AnimatePresence initial={false}>
-          {organizationData.map((project, index) => (
-            index === activeIndex && (
-              <motion.div
-                key={project.id}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <Link to={`/organization/${project.id}`}><img src={project.image} alt={project.title} /></Link>
-                <h2>{project.title}</h2>
-              </motion.div>
-            )
+const Carousel = ({ orgs }) => {
+
+  console.log(orgs)
+
+  const organizationData = orgs.map((org) => ({
+    id: org._id,
+    image: org.image,
+    title: org.name,
+  }));
+
+  return (
+    <>
+      <div className='w-full py-8 mx-auto overflow-hidden'>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          spaceBetween={0}
+          coverflowEffect={{
+            rotate: 10,
+            stretch: 40,
+            depth: 200,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          mousewheel={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[EffectCoverflow, Autoplay, Mousewheel, Pagination]}
+          className="mySwiper py-12 w-full"
+          style={{
+            '--swiper-pagination-color': '#FF7067',
+          }}
+        >
+          {organizationData.map((project) => (
+            <SwiperSlide key={project.id} style={{ width: '300px', height: '200px' }}>
+              <div className="w-full h-full">
+                <h2 className="absolute">{project.title}</h2>
+                <Link to={`/organization/${project.id}`}>
+                  <img className="block w-full h-full" src={project.image} alt={project.title} />
+                </Link>
+              </div>
+            </SwiperSlide>
           ))}
-        </AnimatePresence>
+        </Swiper>
       </div>
-    )
+    </>
+  )
 };
 
 export default Carousel;
