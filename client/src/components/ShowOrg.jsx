ad0cd8aa8f8477534689bbce64f6263536232f84
Cam <camhack67@gmail.com>
2023-10-09 14:10:00 -0400

    create org css

--- client/src/components/ShowOrg.jsx
+++ client/src/components/ShowOrg.jsx
@@ -5,34 +5,43 @@ import ProgressBar from '../components/ProgressBar'
 import images from '../utils/importPhotos'
 
 function ShowOrg(props) {
-  const {loading:orgLoading, data:organizationData} = useQuery(
+  const { loading: orgLoading, data: organizationData } = useQuery(
     GET_SINGLE_ORGANIZATION,
     {
       variables: { organizationId: props.property },
     }
   );
-  const orgData = organizationData?.getSingleOrganization||{};
+  const orgData = organizationData?.getSingleOrganization || {};
   console.log(orgData)
-  
-  const {loading:tagLoading, data:tagData} = useQuery(
+
+  const { loading: tagLoading, data: tagData } = useQuery(
     GET_SINGLE_TAG,
     {
       variables: { tagId: orgData.tag },
     }
   );
-console.log("tagData: ",tagData)
+  console.log("tagData: ", tagData)
   return (
     <>
-      <p>{orgData.name}</p>
-      <img src={orgData.image} style={{ width: '300px'}} alt={orgData.name + "profile image"} />
+      <div className="md:container 2xl:w-2/3 mx-auto flex flex-col justify-center items-center">
 
-      <img src={images[tagData?.getSingleTag?.image]} alt="" />
-      <p>FundraisingGoal:</p>
-      <p>${orgData.fundraisingAmount} / ${orgData.fundraisingGoal}</p> 
-      <p></p>
-      
-      <ProgressBar goal={orgData.fundraisingGoal} amount={orgData.fundraisingAmount} />
-      
+        <img
+          src={orgData.image} style={{ width: '300px' }} alt={orgData.name + "profile image"} />
+
+        <h3 className="font-main text-xl text-center my-2"
+        >{orgData.name}</h3>
+
+        <img className=""
+          src={images[tagData?.getSingleTag?.image]} alt="" />
+
+        <h3 className="font-main text-lg mt-2"
+        >FundraisingGoal</h3>
+
+        <h3 className="font-main text-lg"
+        >${orgData.fundraisingAmount} / ${orgData.fundraisingGoal}</h3>
+
+        <ProgressBar goal={orgData.fundraisingGoal} amount={orgData.fundraisingAmount} />
+      </div>
     </>
   );
 }
