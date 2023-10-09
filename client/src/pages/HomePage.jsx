84b8af8005750dae51f978824305d462a4bd5b00
Cam <camhack67@gmail.com>
2023-10-09 19:45:13 -0400

    page transitions

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import ForMe from '../components/ForMe'
 import Auth from '../utils/auth';
 import { GET_ORGANIZATIONS } from '../utils/queries'
 import { useQuery } from '@apollo/client';
+import { motion, AnimatePresence } from "framer-motion"
 
 
 
@@ -30,45 +31,50 @@ const HomePage = () => {
 
   return (
     <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { ease: "easeOut", duration: 0.6 } }}
+        exit={{ opacity: 0, transition: { ease: "easeIn", duration: 0.6 } }}>
 
-      {Auth.loggedIn() && (
-        <>
-          {/* for you carousel */}
-          <ForMe organizations={organizations} />
-        </>
-      )}
+        {Auth.loggedIn() && (
+          <>
+            {/* for you carousel */}
+            <ForMe organizations={organizations} />
+          </>
+        )}
 
-      <div className='md:container 2xl:w-2/3 mx-auto'>
-        <h2 className='font-main text-primary text-2xl pt-8 md:pt-12 text-center'>More from the community</h2>
-      </div>
+        <div className='md:container 2xl:w-2/3 mx-auto'>
+          <h2 className='font-main text-primary text-2xl pt-8 md:pt-12 text-center'>More from the community</h2>
+        </div>
 
-      <div className='md:container 2xl:w-2/3 mx-auto flex flex-wrap justify-center items-center'>
-        {firstFewElements.map((organization) => {
-          return (
-            <div key={organization._id}>
-              <Link to={`/organization/${organization._id}`}>
-                <div className='relative m-4 md:m-6 w-80 bg-light-1 rounded-lg hover:scale-105 transition-all duration-300 shadow-lg shadow-black/10 hover:shadow-2xl'>
-                  <img className='w-full rounded-t-lg'
-                    src={organization.image} alt="organization profile image" />
+        <div className='md:container 2xl:w-2/3 mx-auto flex flex-wrap justify-center items-center'>
+          {firstFewElements.map((organization) => {
+            return (
+              <div key={organization._id}>
+                <Link to={`/organization/${organization._id}`}>
+                  <div className='relative m-4 md:m-6 w-80 bg-light-1 rounded-lg hover:scale-105 transition-all duration-300 shadow-lg shadow-black/10 hover:shadow-2xl'>
+                    <img className='w-full rounded-t-lg'
+                      src={organization.image} alt="organization profile image" />
 
-                  <div className="absolute bottom-12 w-full">
-                    <ProgressBar instance={organization._id} goal={organization.fundraisingGoal} amount={organization.fundraisingAmount} />
+                    <div className="absolute bottom-12 w-full">
+                      <ProgressBar instance={organization._id} goal={organization.fundraisingGoal} amount={organization.fundraisingAmount} />
+                    </div>
+
+                    <h2 className='font-main text-text-dark text-center pt-4'
+                    >{organization.name}</h2>
+
+                    <h3 className='font-secondary text-text-dark text-center pb-1'
+                    ><span className='font-main text-xl text-secondary'
+                    >${organization.fundraisingAmount}</span> Raised of <span className='font-main text-xl text-secondary'
+                    >${organization.fundraisingGoal}</span> Goal</h3>
+
                   </div>
-
-                  <h2 className='font-main text-text-dark text-center pt-4'
-                  >{organization.name}</h2>
-
-                  <h3 className='font-secondary text-text-dark text-center pb-1'
-                  ><span className='font-main text-xl text-secondary'
-                  >${organization.fundraisingAmount}</span> Raised of <span className='font-main text-xl text-secondary'
-                  >${organization.fundraisingGoal}</span> Goal</h3>
-
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </motion.div>
     </>
   );
 };
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -21,101 +21,106 @@ const ProfilePage = () => {
   }
   return (
     <>
-      <h2 className='font-main text-text-dark text-3xl text-center opacity-50'
-      >Welcome back {userData.username}!</h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { ease: "easeOut", duration: 0.6 } }}
+        exit={{ opacity: 0, transition: { ease: "easeIn", duration: 0.6 } }}>
 
-      {/*conditional rendering based on whether profile is an admin or a user */}
-      {userData.isAdmin === false ? (
-        // is user profile
-        <>
+        <h2 className='font-main text-text-dark text-3xl text-center opacity-50'
+        >Welcome back {userData.username}!</h2>
 
-          <div className='bg-light-2 mt-4'>
+        {/*conditional rendering based on whether profile is an admin or a user */}
+        {userData.isAdmin === false ? (
+          // is user profile
+          <>
 
-            <h3 className='font-main text-primary text-2xl py-4 text-center'
-            >Saved Organizations</h3>
+            <div className='bg-light-2 mt-4'>
 
-            <Carousel orgs={userData.savedOrganizations} />
+              <h3 className='font-main text-primary text-2xl py-4 text-center'
+              >Saved Organizations</h3>
 
-          </div>
+              <Carousel orgs={userData.savedOrganizations} />
 
-          <h3 className='font-main text-primary text-2xl pt-8 md:pt-12 text-center'
-          >Order History</h3>
-
-          <div className='md:container 2xl:w-2/3 mx-auto flex flex-col-reverse justify-center items-center'>
-            {userData.orderHistory.map((order) => {
-              return (
-                <>
-                  <div className='bg-light-1 m-2 p-4 rounded-lg w-2/3 shadow-2xl'>
-
-                    <h3 className='text-center text-2xl py-1'
-                    >{order.organizationName}</h3>
-
-                    <h3 className='text-center text-xl py-1 text-green-600'
-                    >${order.orderTotal}</h3>
-
-                    <h3 className='text-center py-1'
-                    >Completed on: {order.orderDate}</h3>
-
-                  </div>
-                </>
-              );
-            })}
-          </div>
-        </>
-      ) : (
-        // is admin profile
-        <div>
-
-          {userData.myOrganizationId
-            ?
-            <div className='bg-light-2 pb-8 mt-4'>
-              <h2 className='font-main text-primary text-3xl py-4 text-center'
-              >My Organization</h2>
-              <ShowOrg property={userData.myOrganizationId} />
             </div>
-            :
-            <div className="createOrgButton mt-8">
-              <div className='md:container 2xl:w-2/3 mx-auto flex flex-col justify-center items-center mb-4'>
 
-                <h3 className='font-secondary text-text-dark text-xl py-1 text-center'
-                >It doesn't look like you've set up an organization yet...</h3>
+            <h3 className='font-main text-primary text-2xl pt-8 md:pt-12 text-center'
+            >Order History</h3>
 
-                <h3 className='font-secondary text-text-dark text-xl py-1 text-center'
-                >Click the button below to get started on your fundraising project!</h3>
+            <div className='md:container 2xl:w-2/3 mx-auto flex flex-col-reverse justify-center items-center'>
+              {userData.orderHistory.map((order) => {
+                return (
+                  <>
+                    <div className='bg-light-1 m-2 p-4 rounded-lg w-2/3 shadow-2xl'>
 
-                <button className='font-secondary py-2 px-6 my-4 rounded-lg bg-primary hover:bg-secondary text-light-1 transition-all'
-                  onClick={() => setOrgSetupOpen(true)}>Setup Organization</button>
+                      <h3 className='text-center text-2xl py-1'
+                      >{order.organizationName}</h3>
 
+                      <h3 className='text-center text-xl py-1 text-green-600'
+                      >${order.orderTotal}</h3>
+
+                      <h3 className='text-center py-1'
+                      >Completed on: {order.orderDate}</h3>
+
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          // is admin profile
+          <div>
+
+            {userData.myOrganizationId
+              ?
+              <div className='bg-light-2 pb-8 mt-4'>
+                <h2 className='font-main text-primary text-3xl py-4 text-center'
+                >My Organization</h2>
+                <ShowOrg property={userData.myOrganizationId} />
               </div>
-            </div>
-          }
+              :
+              <div className="createOrgButton mt-8">
+                <div className='md:container 2xl:w-2/3 mx-auto flex flex-col justify-center items-center mb-4'>
 
-        </div>
-      )}
+                  <h3 className='font-secondary text-text-dark text-xl py-1 text-center'
+                  >It doesn't look like you've set up an organization yet...</h3>
 
-      <AnimatePresence>
-        {orgSetupOpen && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { ease: "easeOut", duration: 0.2 } }}
-            exit={{ opacity: 0, transition: { ease: "easeIn", duration: 0.2 } }}
-            className='absolute top-0 w-full h-full z-10'>
+                  <h3 className='font-secondary text-text-dark text-xl py-1 text-center'
+                  >Click the button below to get started on your fundraising project!</h3>
 
-            <div onClick={() => setOrgSetupOpen(false)}
-              className='absolute top-0 w-full h-full bg-black opacity-50' />
+                  <button className='font-secondary py-2 px-6 my-4 rounded-lg bg-primary hover:bg-secondary text-light-1 transition-all'
+                    onClick={() => setOrgSetupOpen(true)}>Setup Organization</button>
 
-            <motion.div
-              initial={{ scale: 0.75 }}
-              animate={{ scale: 1, transition: { ease: "easeOut", duration: 0.2 } }}
-              exit={{ scale: 0.75, transition: { ease: "easeIn", duration: 0.2 } }}>
-              <CreateOrgForm />
-            </motion.div>
+                </div>
+              </div>
+            }
 
-          </motion.div>
+          </div>
         )}
-      </AnimatePresence>
 
+        <AnimatePresence>
+          {orgSetupOpen && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { ease: "easeOut", duration: 0.2 } }}
+              exit={{ opacity: 0, transition: { ease: "easeIn", duration: 0.2 } }}
+              className='absolute top-0 w-full h-full z-10'>
 
+              <div onClick={() => setOrgSetupOpen(false)}
+                className='absolute top-0 w-full h-full bg-black opacity-50' />
+
+              <motion.div
+                initial={{ scale: 0.75 }}
+                animate={{ scale: 1, transition: { ease: "easeOut", duration: 0.2 } }}
+                exit={{ scale: 0.75, transition: { ease: "easeIn", duration: 0.2 } }}>
+                <CreateOrgForm />
+              </motion.div>
+
+            </motion.div>
+          )}
+        </AnimatePresence>
+
+      </motion.div>
     </>
   );
 };
