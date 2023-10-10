1327c888ae240f431f9649baeb5bc179c88ded7b
Cam <camhack67@gmail.com>
2023-10-09 20:34:27 -0400

    carousel tweak

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -53,9 +53,8 @@ const Carousel = ({ orgs }) => {
             <SwiperSlide key={project.id} style={{ width: '400px', height: '300px' }}>
               <div className="w-full h-full">
                 <Link to={`/organization/${project.id}`}>
-                <h2 className="absolute">{project.title}</h2>
                   <div className="block w-full h-full">
-                    <img className="absolute top-0 left-0 w-full h-full z-[-1] bg-black object-scale-down rounded-xl" src={project.image} alt={project.title} />
+                    <img className="absolute top-0 left-0 w-full h-full z-[-1] bg-black object-scale-down md:rounded-xl" src={project.image} alt={project.title} />
                   </div>
                 </Link>
               </div>
